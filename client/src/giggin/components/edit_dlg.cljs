f55d6f6a48c14620198079cb1f01b6069ce7ce10
Alon Pinto <pintoalon@gmail.com>
2024-01-09 08:33:41 +0200

    update ui

--- client/src/giggin/components/edit_dlg.cljs
+++ client/src/giggin/components/edit_dlg.cljs
@@ -1,13 +1,26 @@
 (ns giggin.components.edit-dlg)
 
 (defn form-input
-  [{:keys [id type value values]}]
+  [{:keys [id type value values class]}]
   [:div.form__group
-   [:label {:for id} id]
-   [:input {:type type
+   [:label.input-label {:for id} id]
+   [:input {:class class
+            :type type
             :id id
             :value value
             :on-change #(swap! values assoc (keyword id) (.. % -target -value))}]])
+
+
+(defn form-textarea
+  [{:keys [id type value values class]}]
+  [:div.form__group
+   [:label.input-label {:for id} id]
+   [:textarea {:class class
+               :type type
+               :id id
+               :value value
+               :on-change #(swap! values assoc (keyword id) (.. % -target -value))}]])
+
 
 (defn edit-dlg
   [isEditMode values save-gig toggle-modal]
@@ -19,19 +32,22 @@
      [form-input {:id "title"
                   :type "text"
                   :value (:title @values)
-                  :values values}]
-     [form-input {:id "desc"
-                  :type "text"
-                  :value (:desc @values)
-                  :values values}]
+                  :values values
+                  :class "input-lg"}]
+     [form-textarea {:id "desc"
+                     :type "textarea"
+                     :value (:desc @values)
+                     :values values
+                     :class "texterea"}]
      [form-input {:id "price"
                   :type "number"
                   :value (:price @values)
-                  :values values}]]
+                  :values values
+                  :class "input-lg"}]]
     [:div.modal__footer
      [:button.btn.btn--link.float--right
       {:on-click #(toggle-modal {:active false :gig {}})}
       "Cancel"]
      [:button.btn.btn--secondary
-        {:on-click #(save-gig @values)}
+      {:on-click #(save-gig @values)}
       "Save"]]]])
