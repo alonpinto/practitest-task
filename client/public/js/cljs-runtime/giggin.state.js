d99d76d7ff8f3f8e33537992c608eb3877e02e99
Alon Pinto <pintoalon@gmail.com>
2024-01-09 08:32:52 +0200

    remove generate js folder

--- client/public/js/cljs-runtime/giggin.state.js
+++
@@ -1,6 +0,0 @@
-goog.provide('giggin.state');
-giggin.state.orders = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(cljs.core.PersistentArrayMap.EMPTY);
-giggin.state.favorites = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(cljs.core.PersistentVector.EMPTY);
-giggin.state.gigs = reagent.core.atom.cljs$core$IFn$_invoke$arity$1(cljs.core.PersistentArrayMap.EMPTY);
-
-//# sourceMappingURL=giggin.state.js.map
